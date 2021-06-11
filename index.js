3ed80c16f8618fee6140bcad91dc18a40633541a
AlyasKhudier <79033697+Alyask01@users.noreply.github.com>
2021-06-11 01:00:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+// Code your selectRandomEntry function here:
+function selectRandomEntry(arr){
+   let index = Math.floor(Math.random()*arr.length);
+   return arr[index];
+} 
+
+// Code your buildCrewArray function here:
+function buildCrewArray(crew, candidates){
+  let crewNames = [];
+for (i = 0; i < candidates.length; i++){
+ if (crew.includes(candidates[i].astronautID)){
+  
+  crewNames.push(candidates[i].name);
+ }
+}
+return console.log(`${crewNames[0]}, ${crewNames[1]}, and ${crewNames[2]} are going to space!`);
+}
+
+let idNumbers = [291, 414, 503, 599, 796, 890];
+let randomIds = [];
+
+ while (randomIds.length < 3){
+  let chosen = selectRandomEntry(idNumbers);
+  if (randomIds.includes(chosen)){
+    chosen = selectRandomEntry(idNumbers);
+  } else {
+    randomIds.push(chosen);
+  }
+}
+console.log(randomIds);
+// Here are the candidates and the 'animals' array:
+let candidateA = {
+  'name':'Gordon Shumway',
+  'species':'alf',
+  'mass':90,
+  'o2Used':function(hrs){return 0.035*hrs},
+  'astronautID':414
+};
+let candidateB = {
+  'name':'Lassie',
+  'species':'dog',
+  'mass':19.1,
+  'o2Used':function(hrs){return 0.030*hrs},
+  'astronautID':503
+};
+let candidateC = {
+  'name':'Jonsey',
+  'species':'cat',
+  'mass':3.6,
+  'o2Used':function(hrs){return 0.022*hrs},
+  'astronautID':796
+};
+let candidateD = {
+  'name':'Paddington',
+  'species':'bear',
+  'mass':31.8,
+  'o2Used':function(hrs){return 0.047*hrs},
+  'astronautID':291
+};
+let candidateE = {
+  'name':'Pete',
+  'species':'tortoise',
+  'mass':417,
+  'o2Used':function(hrs){return 0.010*hrs},
+  'astronautID':599
+};
+let candidateF = {
+  'name':'Hugs',
+  'species':'ball python',
+  'mass':2.3,
+  'o2Used':function(hrs){return 0.018*hrs},
+  'astronautID':890
+};
+
+let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
+
+// Code your template literal and console.log statements:
+//console.log(selectRandomEntry(animals));
+console.log(buildCrewArray(randomIds,animals));
